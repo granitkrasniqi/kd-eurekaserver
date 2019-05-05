bc4e2cfe79c1f29b6a359b1f672f0dd8104bec96
Granit Krasniqi <krasniqigranit59@gmail.com>
2019-05-05 23:50:25 +0200

    first commit

---
+++ src/main/java/com/karteladentare/kdeurekaserver/KdEurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.karteladentare.kdeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdEurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdEurekaserverApplication.class, args);
+    }
+
+}

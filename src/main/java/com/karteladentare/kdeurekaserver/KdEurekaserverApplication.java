9c0515fbc93cf2a42c63136c6b6b2089107a9d5c
Granit Krasniqi <krasniqigranit59@gmail.com>
2019-05-05 23:56:21 +0200

    @EnableEurekaServer annotation added

--- src/main/java/com/karteladentare/kdeurekaserver/KdEurekaserverApplication.java
+++ src/main/java/com/karteladentare/kdeurekaserver/KdEurekaserverApplication.java
@@ -2,8 +2,10 @@ package com.karteladentare.kdeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class KdEurekaserverApplication {
 
     public static void main(String[] args) {
